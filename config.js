febc2b1d822379a92b19c509da9f6f48da98b030
vnvesports <85272615+vnvesports@users.noreply.github.com>
2021-06-03 12:06:10 +0700

    Create config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ const CONFIG = {
     btnYes: 'Vẫn cứ là thích anh <33
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply : Gửi    cho  anh 
+    reply : 'Yêu thì yêu mà không yêu thì yêu <3333333
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
